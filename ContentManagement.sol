e91f994c1aadd309ff6c766887a21d6031110c05
Chiara <baragliachiara@yahoo.it>
2018-06-12 23:06:17 +0200

    ContentManagement refactoring

--- ContentManagement.sol
+++ ContentManagement.sol
@@ -2,36 +2,6 @@ pragma solidity 0.4.23;
 
 import "./BaseContentManagement.sol";
 
-contract MusicContentManagement is BaseContentManagement{
-
-    bytes32 private encoding;
-    uint private bitrate;
-    uint private duration;
-    
-    constructor(bytes32 _title, bytes32 _author, bytes32 _encoding, uint _bitrate, uint _duration, address _catalogAddress) public{
-        title = _title;
-        author = _author;
-        genre = "736f6e67";
-        encoding = _encoding;
-        bitrate = _bitrate;
-        duration = _duration;
-        catalogAddress = _catalogAddress;
-        catalog = Catalog(catalogAddress);
-        catalog.LinkToTheCatalog(title);
-    }
-
-    function consumeContent() external onlyIfAllowed() returns(bytes32 _encoding, uint _bitrate, uint _duration){
-        allowedUsers[msg.sender] = false;
-        if(catalog.IsPremium(msg.sender)==false){
-            catalog.AddViews(title);
-        }
-
-        _encoding = encoding;
-        _bitrate = bitrate;
-        _duration = duration;
-    }
-}
-
 contract BookContentManagement is BaseContentManagement{
 
     bytes32 private encoding;
@@ -41,21 +11,24 @@ contract BookContentManagement is BaseContentManagement{
         title = _title;
         author = _author;
         genre = "626f6f6b";
-        encoding = _encoding;
-        pages = _pages;
+        content.push(_encoding);
+        content.push(bytes32(_pages));
         catalogAddress = _catalogAddress;
         catalog = Catalog(catalogAddress);
         catalog.LinkToTheCatalog(title);
     }
 
-    function consumeContent() external onlyIfAllowed() returns(bytes32 _encoding, uint _pages){
+    function grantAccess(address _user) external isCatalog{
+        allowedUsers[_user] = true;
+    }
+
+    function consumeContent() external onlyIfAllowed() returns(bytes32[]){
         allowedUsers[msg.sender] = false;
         if(catalog.IsPremium(msg.sender)==false){
             catalog.AddViews(title);
         }
 
-        _encoding = encoding;
-        _pages = pages;
+        return content;
     }
 }
 
@@ -67,31 +40,61 @@ contract MovieContentManagement is BaseContentManagement{
     uint private width;
     uint private height;
     
-    constructor(bytes32 _title, bytes32 _author, bytes32 _encoding, uint _bitrate, uint _duration, 
-    uint _width, uint _height, address _catalogAddress) public{
+    constructor(bytes32 _title, bytes32 _author, bytes32 _encoding, uint _bitrate, uint _duration,
+    uint _resolution, address _catalogAddress) public{
         title = _title;
         author = _author;
         genre = "6d6f766965";
-        encoding = _encoding;
-        bitrate = _bitrate;
-        duration = _duration;
-        width = _width;
-        height = _height;
+        content.push(_encoding);
+        content.push(bytes32(_bitrate));
+        content.push(bytes32(_duration));
+        content.push(bytes32(_resolution));
         catalogAddress = _catalogAddress;
         catalog = Catalog(catalogAddress);
         catalog.LinkToTheCatalog(title);
     }
 
-    function consumeContent() external onlyIfAllowed() returns(bytes32 _encoding, uint _bitrate, uint _duration, uint _width, uint _height){
+    function grantAccess(address _user) external isCatalog{
+        allowedUsers[_user] = true;
+    }
+
+    function consumeContent() external onlyIfAllowed() returns(bytes32[]){
         allowedUsers[msg.sender] = false;
         if(catalog.IsPremium(msg.sender)==false){
             catalog.AddViews(title);
         }
 
-        _encoding = encoding;
-        _bitrate = bitrate;
-        _duration = duration;
-        _width = width;
-        _height = height;
+        return content;
+    }
+}
+
+contract MusicContentManagement is BaseContentManagement{
+
+    bytes32 private encoding;
+    uint private bitrate;
+    uint private duration;
+    
+    constructor(bytes32 _title, bytes32 _author, bytes32 _encoding, uint _bitrate, uint _duration, address _catalogAddress) public{
+        title = _title;
+        author = _author;
+        genre = "736f6e67";
+        content.push(_encoding);
+        content.push(bytes32(_bitrate));
+        content.push(bytes32(_duration));
+        catalogAddress = _catalogAddress;
+        catalog = Catalog(catalogAddress);
+        catalog.LinkToTheCatalog(title);
+    }
+
+    function grantAccess(address _user) external isCatalog{
+        allowedUsers[_user] = true;
+    }
+
+    function consumeContent() external onlyIfAllowed() returns(bytes32[]){
+        allowedUsers[msg.sender] = false;
+        if(catalog.IsPremium(msg.sender)==false){
+            catalog.AddViews(title);
+        }
+        return content;
     }
 }
